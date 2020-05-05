32fe7afe9bf6f2e4963315cc8752e7e43de403a7
ld8 <don_lee@me.com>
2020-05-05 18:44:56 +0800

    Updates

---
+++ precache-manifest.910bb3483de0c134138bcc50c15dcfe3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e31ae90e844338e175731a9fdd8b9518",
+    "url": "/react-rock-paper-scissors/index.html"
+  },
+  {
+    "revision": "f3ac43c3a4efd10d836c",
+    "url": "/react-rock-paper-scissors/static/css/main.7d608c0a.chunk.css"
+  },
+  {
+    "revision": "804887596ee23a53502b",
+    "url": "/react-rock-paper-scissors/static/js/2.cfe0896c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rock-paper-scissors/static/js/2.cfe0896c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3ac43c3a4efd10d836c",
+    "url": "/react-rock-paper-scissors/static/js/main.92b7a14c.chunk.js"
+  },
+  {
+    "revision": "61506f55760f287605ed",
+    "url": "/react-rock-paper-scissors/static/js/runtime-main.19644656.js"
+  },
+  {
+    "revision": "19699e5afee2a88a6da009af13f21568",
+    "url": "/react-rock-paper-scissors/static/media/paper.19699e5a.png"
+  },
+  {
+    "revision": "f23b4798e1bfe33a3028c9a0a267e10d",
+    "url": "/react-rock-paper-scissors/static/media/rock.f23b4798.png"
+  },
+  {
+    "revision": "5621f9924bfed089a39968b1fd9ebfdc",
+    "url": "/react-rock-paper-scissors/static/media/scissors.5621f992.png"
+  }
+]);
